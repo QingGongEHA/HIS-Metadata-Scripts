64340bd540529ab7905d7a9ea555fdbbc6a736e8
QingGongEHA <33089623+QingGongEHA@users.noreply.github.com>
2018-03-28 16:18:22 +0000

    Update JsonToHtml.py

--- JsonToHtml.py
+++ JsonToHtml.py
@@ -7,7 +7,7 @@ file_json = open('D:\MoH Interoperability\HIS Metadata\HMIS-DHIS2\DHIS2 Metadata
 data_json = json.load(file_json)
 result_html = json2html.convert(json = data_json, table_attributes="class=\"table table-bordered table-hover\"").encode('utf-8')
 
-file_html = open('D:\MoH Interoperability\HIS Metadata\HMIS-DHIS2\DHIS2 Metadata-splitted\\dashboardItems.html','w') #replace with the path where you want to see the result, replace the file name with what you want to call the result
+file_html = open('D:\MoH Interoperability\HIS Metadata\HMIS-DHIS2\DHIS2 Metadata-splitted\\dashboardItems.html','w') #replace with the path where you want to see the result, replace the file name with how you want to name the result
 file_html.write(result_html)
 
 file_html.close()
